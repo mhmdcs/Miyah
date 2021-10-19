6c507229930069555a3ca7ae31b941b3e982ffb2
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-10-19 21:44:45 +0300

    register Firebase database

--- app/src/main/java/com/example/miyah/MainActivity.kt
+++ app/src/main/java/com/example/miyah/MainActivity.kt
@@ -2,6 +2,13 @@ package com.example.miyah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
