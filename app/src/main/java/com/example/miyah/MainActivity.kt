cfa53c6c59145c5f72610c84097b7425fe998f9c
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-10-30 14:22:18 +0300

    fix navigation error

--- app/src/main/java/com/example/miyah/MainActivity.kt
+++ app/src/main/java/com/example/miyah/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.miyah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.miyah.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -11,8 +13,11 @@ import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
