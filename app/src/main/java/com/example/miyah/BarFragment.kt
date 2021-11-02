1ab4087baeaac1f386b7f5d2fac91d52620e869f
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-02 23:07:56 +0300

    working version of the app with firebase

--- app/src/main/java/com/example/miyah/BarFragment.kt
+++ app/src/main/java/com/example/miyah/BarFragment.kt
@@ -13,10 +13,16 @@ import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
 import com.example.miyah.databinding.FragmentBarBinding
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class BarFragment : Fragment() {
 
     private lateinit var binding: FragmentBarBinding
+    private lateinit var database: DatabaseReference
+    val TAG = "LogBarFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +30,25 @@ class BarFragment : Fragment() {
     ): View? {
 
         //initialize the data binding variable
-        val binding: FragmentBarBinding = DataBindingUtil.inflate(
+         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_bar,
             container,
             false)
+
+        var status: String
+        database = FirebaseDatabase.getInstance().reference
+
+        database.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+            status = snapshot.child("distances").value.toString()
+                binding.textWaterPercentage.text = status
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
 
         return binding.root
     }
