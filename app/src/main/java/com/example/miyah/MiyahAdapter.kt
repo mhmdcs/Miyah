ae099356c91b9a155f15b6c64187cb1a6716b671
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-11-13 03:09:27 +0300

    images in provider fragment launch google maps with clients location

--- app/src/main/java/com/example/miyah/MiyahAdapter.kt
+++ app/src/main/java/com/example/miyah/MiyahAdapter.kt
@@ -1,11 +1,20 @@
 package com.example.miyah
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.miyah.databinding.ClientItemBinding
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import androidx.core.content.ContextCompat.startActivity
+
+import android.content.Intent
+import androidx.core.content.ContextCompat
+
 
 class MiyahAdapter(options: FirebaseRecyclerOptions<User>) :
     FirebaseRecyclerAdapter<User, MiyahAdapter.ClientsViewHolder>(options) {
@@ -13,7 +22,8 @@ class MiyahAdapter(options: FirebaseRecyclerOptions<User>) :
     class ClientsViewHolder(private val binding: ClientItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val name = binding.clientName
         val phone = binding.clientPhone
-        val location = binding.clientLocation
+        val locationImage = binding.clientLocationImage
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClientsViewHolder {
@@ -24,7 +34,21 @@ class MiyahAdapter(options: FirebaseRecyclerOptions<User>) :
     override fun onBindViewHolder(holder: ClientsViewHolder, position: Int, model: User) {
         holder.name.text = model.name
         holder.phone.text = model.phone
-        holder.location.text = model.location
+//        val uriString = "https://www.google.com/maps/search/?api=1&query="+model.location
+//        val locationUri = uriString.toUri()
+//        holder.locationImage.setImageURI(locationUri)
+
+        holder.locationImage.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                val context = holder.locationImage.context
+                val url = "https://www.google.com/maps/search/?api=1&query="+model.location
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                context.startActivity(intent)
+            }
+
+        })
+
     }
 
 }
